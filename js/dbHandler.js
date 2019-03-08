585305922bef50d1d16bc0b6365a37bfd1a26090
amcclead <amcclead@gmail.com>
2019-03-08 13:46:01 -0600

    add save and add new item

--- js/dbHandler.js
+++ js/dbHandler.js
@@ -1,7 +1,17 @@
 //Purpose of this file is this file is to add new items to the bid page and update items. 
 
 function callDataBase() {
-  bidDBJSON = JSON.parse(localStorage.getItem('bidDB'))
+  let bidDBJSON = JSON.parse(localStorage.getItem('bidDB'));
   return bidDBJSON;
 }
 
+function saveDataBase(db) {
+  localStorage.setItem('bidDB',(JSON.stringify(db));
+  console.log("Data saved");
+}
+
+function addNewItem(obj) {
+  let db = callDataBase();
+  db.items.push(obj);
+  saveDataBase(db)
+}
