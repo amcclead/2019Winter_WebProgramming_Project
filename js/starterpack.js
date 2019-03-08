d1b79a36324f48be1683abd6e19aef667df863a0
amcclead <amcclead@gmail.com>
2019-03-08 12:08:37 -0600

    Add new entries to starterPack

--- js/starterpack.js
+++ js/starterpack.js
@@ -1,12 +1,33 @@
+const starterPack= {
+      items:[
+      {createdBy:"Indy", 
+       title:"Lost Arc", 
+       Des:"Long lost ancient artifact.", 
+       startingPrice:1, 
+       currentBid:0, 
+       bidderName:"Unknown"},
+        {createdBy:"Luke", 
+       title:"Lightsaber", 
+       Des:"Need some money. Being a Jedi doesn't pay much.", 
+       startingPrice:1000000, 
+       currentBid:5, 
+       bidderName:"Han"},
+        {createdBy:"Stranger", 
+       title:"Magic Beans", 
+       Des:"Need a new cow", 
+       startingPrice:10, 
+       currentBid:10, 
+       bidderName:"Jack"}
+        ]
+        }
+
 if (typeof(Storage) !== "undefined") {
   if (localStorage.bidStarterPack){
 
   }
 
   else {
-    starterPack= {
-      {pos:1, createdBy:"Indy", title:"Lost Arc", Des:"Long lost ancient artifact.", startingPrice:1, currentBid:0, bidderName:""}
-    }
+
   }
 } else {
   // Sorry! No Web Storage support..
