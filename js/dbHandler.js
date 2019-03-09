ec899d755b89027634c2298ba89a97641c58a9f8
amcclead <amcclead@gmail.com>
2019-03-09 11:48:34 -0600

    Successful intergration of handlebars and database recall.

--- js/dbHandler.js
+++ js/dbHandler.js
@@ -1,17 +1,25 @@
-//Purpose of this file is this file is to add new items to the bid page and update items. 
+//Purpose of this file is this file is to add new items to the bid page and update items.
 
 function callDataBase() {
   let bidDBJSON = JSON.parse(localStorage.getItem('bidDB'));
+  console.log("bidDBJSON: "+bidDBJSON);
   return bidDBJSON;
 }
 
 function saveDataBase(db) {
-  localStorage.setItem('bidDB',(JSON.stringify(db));
+  localStorage.setItem('bidDB',(JSON.stringify(db)));
   console.log("Data saved");
 }
 
 function addNewItem(obj) {
   let db = callDataBase();
   db.items.push(obj);
-  saveDataBase(db)
+  saveDataBase(db);
 }
+
+
+let db = callDataBase();
+let bidTemplate = document.querySelector("#bid-template").innerHTML;
+let compiledTemplate= Handlebars.compile(bidTemplate);
+document.querySelector("#bid-list-container").innerHTML = compiledTemplate(db.items[0]);
+console.log("hello world")
--- js/starterpack.js
+++ js/starterpack.js
@@ -1,23 +1,23 @@
 const starterPack= {
-      items:[
-      {createdBy:"Indy", 
-       title:"Lost Arc", 
-       Des:"Long lost ancient artifact.", 
-       startingPrice:1, 
-       currentBid:0, 
-       bidderName:"Unknown"},
-        {createdBy:"Luke", 
-       title:"Lightsaber", 
-       Des:"Need some money. Being a Jedi doesn't pay much.", 
-       startingPrice:1000000, 
-       currentBid:5, 
-       bidderName:"Han"},
-        {createdBy:"Stranger", 
-       title:"Magic Beans", 
-       Des:"Need a new cow", 
-       startingPrice:10, 
-       currentBid:10, 
-       bidderName:"Jack"}
+      "items":[
+      {"createdBy":"Indy",
+       "title":"Lost Arc",
+       "Des":"Long lost ancient artifact.",
+       "startingPrice":1,
+       "currentBid":0,
+       "bidderName":"Unknown"},
+        {"createdBy":"Luke",
+       "title":"Lightsaber",
+       "Des":"Need some money. Being a Jedi doesn't pay much.",
+       "startingPrice":1000000,
+       "currentBid":5,
+       "bidderName":"Han"},
+        {"createdBy":"Stranger",
+       "title":"Magic Beans",
+       "Des":"Need a new cow",
+       "startingPrice":10,
+       "currentBid":10, 
+       "bidderName":"Jack"}
       ]
 }
 
